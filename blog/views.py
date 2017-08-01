b621e4d226cd9ca0601368fb0b58988e94828337
emiliaYM <yingmin809@gmail.com>
2017-08-01 18:02:30 +0800

    modified template

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+#impoort timezone
+from django.utils import timezone
+#The dot before models means current directory or current application. 
+#Both views.py and models.py are in the same directory. This means we can use . 
+#and the name of the file (without .py). Then we import the name of the model (Post).
+from .models import Post
+
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
