708ee5f88785aee7d9ded2a31f47eb9d6673a33f
emiliaYM <yingmin809@gmail.com>
2017-08-02 12:40:56 +0800

    add post detail page

--- blog/views.py
+++ blog/views.py
@@ -6,8 +6,15 @@ from django.utils import timezone
 #and the name of the file (without .py). Then we import the name of the model (Post).
 from .models import Post
 
+from django.shortcuts import render, get_object_or_404
+
 
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
